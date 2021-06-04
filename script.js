71c31e1064ab17db35aa30a6d6de005812df6469
nbogie <neillbogie@googlemail.com>
2021-06-04 15:04:13 +0100

    add js

---
+++ script.js
@@ -0,0 +1,16 @@
+function rotateTheImage() {
+  const firstImage = document.querySelector("img");
+  firstImage.style.transform = "rotate(45deg)";
+  console.log("Hopefully, the image's rotation was set to 45%");
+  console.log("By the way the time is now " + new Date());
+}
+
+function prepareButton() {
+  //Find the button by its ID, keep a reference to it
+  const button = document.querySelector("#coolButton");
+
+  //Register a function to call when the button is clicked
+  button.addEventListener("click", rotateTheImage);
+}
+
+window.onload = prepareButton; //the function to call when the page has finished loading.
