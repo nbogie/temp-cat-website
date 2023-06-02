5127489d19b52627914b9fbd87e46ae46d93df1b
nbogie <neillbogie@googlemail.com>
2023-06-02 23:44:23 +0100

    minor improvements to script

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function rotateTheImage() {
+function rotateFirstImage() {
+  //Find the first element with a tag of "img"; Keep a reference to it.
   const firstImage = document.querySelector("img");
+  //Change its style properties, causing it to be rotated
   firstImage.style.transform = "rotate(45deg)";
+
   console.log("Hopefully, the image's rotation was set to 45%");
   console.log("By the way the time is now " + new Date());
 }
 
-//Find the button by its ID, keep a reference to it
-const button = document.querySelector("#coolButton");
-
+//Find the button by its ID; Keep a reference to it.
+const myButton = document.getElementById("coolButton");
 //Register a function to call when the button is clicked
-button.addEventListener("click", rotateTheImage);
+myButton.addEventListener("click", rotateFirstImage);
